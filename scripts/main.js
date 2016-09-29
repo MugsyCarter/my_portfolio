23fe11ea63696bcaabd5ce7de1d37b9b89184bdd
Mugsy Carter <mugsycarter@gmail.com>
2016-09-28 17:01:30 -0700

    Some layout issues fixed.  Working buttons Added.

--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,61 @@
 'use strict';
 //on page load the elements with the 'vis' attribute are made visible
 $(document).ready(function(){
-  $('div:not(".head")').slideUp();;
-  $('.body').delay(400).slideDown('slow');
-  $('.about').delay(700).slideDown('slow');
-  $('.foot').delay(900).slideDown('slow');
+  $('[data-more="less"]').hide();
+  $('div:not(".head")').slideUp();
+  $('.body').delay(300).slideDown('slow');
+  $('.about').delay(500).slideDown('slow');
+  $('.foot').delay(600).slideDown('slow');
 });
 
 
   //This function displays the hidden content when clicked
 function openBar(){
   $('div[data-vis="vis"]').on('click', function(){
-    $(this).slideUp('slow');
-    $(this).prev().slideDown('slow');
+    $(this).slideUp('fast');
+    $(this).prev().slideDown('fast');
+
   });
 };
 
 //This function displays the original categry again when clicked
 function closeBar(){
-  $('div[data-vis="invis"]').on('click', function(){
-    $(this).slideUp('slow');
-    $(this).next().slideDown('slow');
+  $('div[data-vis="invis"]').not('.gallery').on('click', function(){
+    $(this).slideUp('fast');
+    $(this).next().slideDown('fast');
   });
 };
 
 function openGallery(){
   $('.body').on('click', function(){
-    $(this).slideUp('slow');
-    $('.gallery').css('height', '80vh');
-    $('div').not('.gallery, .site-holder').css('height', '5vh');
-    $('.gallery').slideDown('slow');
+    $(this).slideUp('fast');
+    $('.gallery').css('min-height', '80vh');
+    $('div').not('.gallery, .site-holder').css('min-height', '5vh');
+    $('.gallery').slideDown('fast');
     $('.site-holder').show();
+    $('article.template').hide();
   });
 };
 
 function closeGallery(){
-  $('.gallery').on('click', function(){
-    $(this).slideUp('slow');
-    $('.body').slideDown('slow');
+  $('.close-gallery').on('click', function(){
+    $('.gallery').slideUp('fast');
+    $('.body').slideDown('fast');
     $('div').css('height', '23vh');
 
   });
 };
 
 
-//this function displays the menu when you click on the 'hamburger'
+function showMore(){
+  $('.more-button').on('click', function(){
+    $('[data-more="less"]').show();
+  });
+};
 
-// function openMenu(){
-//   $('.icon-menu').on('click', function(){
-//     alert('button!');
-//     $('.body').show();
-//   });
-// };
+
 openGallery();
 closeGallery();
 openBar();
 closeBar();
-// openMenu();
+showMore();
